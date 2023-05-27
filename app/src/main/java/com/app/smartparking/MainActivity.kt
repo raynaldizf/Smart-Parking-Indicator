ab532b6e0fbbc852990ab976a17e31dfd578ad6a
Raynaldi Zulfikar <raynaldizoel@gmail.com>
2023-05-27 10:37:35 +0700

    Hide API Url

--- app/src/main/java/com/app/smartparking/MainActivity.kt
+++ app/src/main/java/com/app/smartparking/MainActivity.kt
@@ -2,10 +2,12 @@ package com.app.smartparking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.smartparking.data.network.ApiClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ApiClient.initialize(this)
     }
 }
--- app/src/main/java/com/app/smartparking/data/network/ApiClient.kt
+++ app/src/main/java/com/app/smartparking/data/network/ApiClient.kt
@@ -1,16 +1,31 @@
 package com.app.smartparking.data.network
 
+import android.content.Context
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.util.Properties
 
 object ApiClient {
-    private const val BASE_URL= "https://api-sapi-iot.vercel.app/api/"
+    private var BASE_URL: String = ""
+
+    fun initialize(context: Context) {
+        val properties = Properties()
+        try {
+            val inputStream = context.assets.open("config.properties")
+            properties.load(inputStream)
+            BASE_URL = properties.getProperty("apiBaseUrl")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            // Handle the exception
+        }
+    }
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        retrofit.create(ApiService :: class.java)
+        retrofit.create(ApiService::class.java)
     }
 }
